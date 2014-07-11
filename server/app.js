1441be6b00655596c7bee917719387c0b059e8d5
James Sumners <james.sumners@gmail.com>
2014-07-11 11:20:01 -0400

    Reorganize app.js a bit

--- server/app.js
+++ server/app.js
@@ -3,24 +3,20 @@
 var config = require('./config'),
     express = require('express'),
     morgan = require('morgan'),
+    sqlite3 = require('sqlite3'),
     twig = require('twig');
 
 var app = express(),
-    loadRoutes = function(){};
-
-config = config(app.get('env'));
-console.dir(config);
-
-// Twig configuration
-app.set('view engine', 'twig');
-app.set('view options', {layout: false});
-app.set('twig options', {});
+    loadRoutes = function(){},
+    setupViewConfig = function(){};
 
 function main() {
   var server = {};
 
+  setupViewConfig();
   loadRoutes();
 
+  app.use(morgan()); // Basically equivalent to Apache HTTPD's access.log
   server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
   });
@@ -30,5 +26,14 @@ loadRoutes = function() {
   require('./routes')(app);
 };
 
-app.use(morgan());
+setupViewConfig = function() {
+  // Twig configuration
+  app.set('view engine', 'twig');
+  app.set('view options', {layout: false});
+  app.set('twig options', {});
+};
+
+// Run some blocking operations and then do the main setup and launching
+config = config(app.get('env'));
+app.set('db', new sqlite3.Database(config.db.file));
 main();
