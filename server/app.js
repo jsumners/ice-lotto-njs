8e7a84d7be3b18e5dea0afa417f93e1c4f484923
Carlos Zuñiga <carlos.zun@gmail.com>
2014-07-16 00:29:50 -0500

    Added pasport configuration

--- server/app.js
+++ server/app.js
@@ -4,17 +4,22 @@ var config = require('./config'),
     express = require('express'),
     morgan = require('morgan'),
     sqlite3 = require('sqlite3'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    session = require('express-session'),
     twig = require('twig');
 
 var app = express(),
     loadRoutes = function(){},
-    setupViewConfig = function(){};
+    setupViewConfig = function(){},
+    setupPassport = function(){};
 
 function main() {
   var server = {};
 
   setupViewConfig();
   loadRoutes();
+  setupPassport();
 
   app.use(morgan()); // Basically equivalent to Apache HTTPD's access.log
   server = app.listen(8080, function() {
@@ -31,6 +36,34 @@ setupViewConfig = function() {
   app.set('view engine', 'twig');
   app.set('view options', {layout: false});
   app.set('twig options', {});
+};
+
+setupPassport = function() {
+  //http://passportjs.org/guide/configure/
+  app.use(session(config.session));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(new LocalStrategy(
+    function(username, password, done){
+      // TODO: Find user in database and verify password
+      var user = {id: 1, username: username};
+      if (!user) { // || !verifyPassword(user, password)
+        done(null, false, {message: 'Incorrect Credentials'});
+        return;
+      }
+      done(null, user);
+    }
+  ));
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    // TODO: find user from id
+    var user = {id: 1, username: 'user'};
+    done(null, user);
+  })
 };
 
 // Run some blocking operations and then do the main setup and launching
