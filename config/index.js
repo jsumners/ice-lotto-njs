41f02cb76c7545fe93ad2a7b249e757898a6c605
James Sumners <james.sumners@gmail.com>
2014-07-21 18:43:27 -0400

    Adds support for a data storage directory

--- config/index.js
+++ config/index.js
@@ -1,4 +1,7 @@
+'use strict';
+
 module.exports = function(env) {
+  var fs = require('fs');
   var config = require('./' + env + '.json');
 
   if (process.env.OPENSHIFT_APP_NAME) {
@@ -6,6 +9,24 @@ module.exports = function(env) {
     // Thus, we need to override some things.
     config.network.ip = process.env.OPENSHIFT_NODEJS_IP;
     config.network.port = process.env.OPENSHIFT_NODEJS_PORT;
+    config.dataDir = (function(dataDir) {
+      var components = dataDir.split('/'),
+          dirName = '';
+
+      if (components.slice(components.length - 1)[0] === '') {
+        dirName = components.slice(components.length - 2)[0];
+      } else {
+        dirName = components.slice(components.length - 1)[0];
+      }
+
+      return process.env.OPENSHIFT_DATA_DIR + '/' + dirName + '/';
+    }(config.dataDir));
+  }
+
+  if (!fs.existsSync(config.dataDir)) {
+    fs.mkdirSync(config.dataDir);
+  } else if (!fs.statSync(config.dataDir).isDirectory()) {
+    fs.mkdirSync(config.dataDir);
   }
 
   console.log('config => ', config);
