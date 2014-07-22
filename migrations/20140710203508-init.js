fb4c9bfb41817c295fa14bc30e094aa782996df3
James Sumners <james.sumners@gmail.com>
2014-07-22 15:37:08 -0400

    The db-migrate script requires you to call the "callback"
    When your migration is finished, you absolutely have to invoke the
    passed in callback. Otherwise, the db-migrate script will not complete
    successfully.

--- migrations/20140710203508-init.js
+++ migrations/20140710203508-init.js
@@ -1,51 +1,23 @@
 'use strict';
-var dbm = require('db-migrate');
+var async = require('async'),
+    dbm = require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-    db.runSql(
-      'create table if not exists sessions (id text primary key collate rtrim, data text) without rowid',
-      function(err) {
-        if (err) {
-          console.log('Could not create sessions table: `%s`', err);
-          callback(err);
-          return;
-        }
-
-        db.runSql(
-          'create index if not exists sessions_idx on sessions (id)',
-          function(err) {
-            if (err) {
-              console.log('Could not create sessions index: `%s', err);
-              callback(err);
-              return;
-            }
-          }
-        );
-      }
-    );
-
-    db.runSql(
-      'create table if not exists settings (id integer primary key, name text, data text)',
-      function(err) {
-        if (err) {
-          console.log('Could not create settings table: `%s`', err);
-          callback(err);
-          return;
-        }
-
-        db.runSql(
-          'create index if not exists settings_idx on settings (name)',
-          function(err) {
-            if (err) {
-              console.log('Could not create settings index: `%s`', err);
-              callback(err);
-              return;
-            }
-          }
-        );
-      }
-    );
+  async.series([
+    db.runSql.bind(db,
+      'create table if not exists sessions (id text primary key collate rtrim, data text) without rowid'
+    ),
+    db.runSql.bind(db,
+      'create index if not exists sessions_idx on sessions (id)'
+    ),
+    db.runSql.bind(db,
+      'create table if not exists settings (id integer primary key, name text, data text)'
+    ),
+    db.runSql.bind(db,
+      'create index if not exists settings_idx on settings (name)'
+    )
+  ], callback);
 };
 
 exports.down = function(db, callback) {
