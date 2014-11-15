27239cf76a8c79f6ad19a9aa0b6099d3879b21cc
James Sumners <james.sumners@gmail.com>
2014-11-15 18:46:21 -0500

    Fixes Grunt migrate task so that it works correctly in development
    Need to modify the upstream grunt module so that it can support
    multiple environments, but this is good enough for now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,10 @@ module.exports = function(grunt) {
     migrate: {
       options: {
         config: './migrations/database.json',
-        verbose: true
+        verbose: true,
+        env: {
+          NODE_ENV: 'development'
+        }
       },
       verbose: true
     }
